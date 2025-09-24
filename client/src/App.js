161d98fb769a4ededc08b9f42c87bddc1e8c6986
jmal919316-cloud <jmal919316@gmail.com>
2025-09-24 00:57:25 -1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-const API = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API = process.env.REACT_APP_API_URL || process.env.REACT_APP_API_URL
+
 
 function App() {
   const [products, setProducts] = useState([]);
